65864545d2376a34b783d3ca6a37ceaa95de6be8
Mariana Longhi <marimlonghi2@gmail.com>
2026-07-30 10:33:38 -0300

    versão 1 Mariana

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("oi");
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite sua idade: ");
+        int idade = sc.nextInt();
+        System.out.println("Sua idade é: " +  idade);
     }
 }
+
