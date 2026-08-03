cea906e2db203e1a2170149aa16170403626495b
larissa-dias3 <larissadias.k8@gmail.com>
2026-08-02 23:37:40 -0300

    Criando classe Regra

---
+++ src/Regra.java
@@ -0,0 +1,16 @@
+public class Regra {
+    String nome;
+    String[] palavrasChave;
+    int pontos;
+    String porQueAtencao;
+    String falsoPositivo;
+
+    public Regra(String nome, String[] palavrasChave, int pontos, String porQueAtencao, String falsoPositivo) {
+        this.nome = nome;
+        this.palavrasChave = palavrasChave;
+        this.pontos = pontos;
+        this.porQueAtencao = porQueAtencao;
+        this.falsoPositivo = falsoPositivo;
+    }
+
+}
